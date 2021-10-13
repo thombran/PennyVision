15c8dbf4d8d1a0227a8dd544ea21c6702297fee1
Hephaestos <thedanielfloyd@protonmail.com>
2021-10-13 16:19:41 -0400

    add progress bar and read from arg

--- Penny.py
+++ Penny.py
@@ -1,38 +1,47 @@
 import imageio
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 import math
+import sys
+import os
 
-def main():
-    cropped_read = imageio.imread('./PennyCropNew.jpg') # Loading in image data
-    data_crop = [[0 for x in range(cropped_read.shape[1])] for y in range(cropped_read.shape[0])] # Instantiating empty matrix for values
-    # Iterate through rows and columns of image data_crop and create a data_crop point corresponding to greyscale value
-    for row in range(0, cropped_read.shape[0]):
-        for col in range(0, cropped_read.shape[1]):
-            # Weighted greyscale calculatuon
-            data_crop[row][col] = cropped_read[row, col, 0] * .299 + cropped_read[row, col, 1] * .587 + cropped_read[row, col, 2] * .114
+if len(sys.argv) != 2:
+    print('Please supply an image file\nUsage: python penny.py example.jpg')
+    exit(1)
 
-    full_read = imageio.imread('./PennyInNew.jpg') # Load in image data for full image
-    data_full = [[0 for x in range(full_read.shape[1])] for y in range(full_read.shape[0])]
-    # Convert full image to greyscale values, push to data_full matrix
-    for row in range(0, full_read.shape[0]):
-        for col in range(0, full_read.shape[1]):
-            data_full[row][col] = full_read[row, col, 0] * .299 + full_read[row, col, 1] * .587 + full_read[row, col, 2] * .114
+image = sys.argv[1]
+if not os.path.exists(image):
+    print('Image file is invalid')
+    exit(1)
 
-    e_rows = full_read.shape[0] - cropped_read.shape[0]
-    e_cols = full_read.shape[1] - cropped_read.shape[1]
-    t_rows = cropped_read.shape[0]
-    t_cols = cropped_read.shape[1]
+cropped_read = imageio.imread('./PennyCropNew.jpg') # Loading in image data
+data_crop = [[0 for x in range(cropped_read.shape[1])] for y in range(cropped_read.shape[0])] # Instantiating empty matrix for values
+# Iterate through rows and columns of image data_crop and create a data_crop point corresponding to greyscale value
+for row in range(0, cropped_read.shape[0]):
+    for col in range(0, cropped_read.shape[1]):
+        # Weighted greyscale calculation
+        data_crop[row][col] = cropped_read[row, col, 0] * .299 + cropped_read[row, col, 1] * .587 + cropped_read[row, col, 2] * .114
 
-    euclidean_data = [[0 for x in range(0, e_cols)] for y in range(0, e_rows)]
-    print('data created')
-    for i in range(0, e_rows):
-        for j in range(0, e_cols):
-            for k in range(0, t_rows):
-                for l in range(0, t_cols):
-                    euclidean_data[i][j] += abs(data_full[i+k][j+l] - data_crop[k][l])
+full_read = imageio.imread(image) # Load in image data for full image
+data_full = [[0 for x in range(full_read.shape[1])] for y in range(full_read.shape[0])]
+# Convert full image to greyscale values, push to data_full matrix
+for row in range(0, full_read.shape[0]):
+    for col in range(0, full_read.shape[1]):
+        data_full[row][col] = full_read[row, col, 0] * .299 + full_read[row, col, 1] * .587 + full_read[row, col, 2] * .114
 
-    full_img = plt.imshow(euclidean_data)
-    plt.show()
+e_rows = full_read.shape[0] - cropped_read.shape[0]
+e_cols = full_read.shape[1] - cropped_read.shape[1]
+t_rows = cropped_read.shape[0]
+t_cols = cropped_read.shape[1]
 
+#fill array with 0s
+euclidean_data = [[0 for x in range(0, e_cols)] for y in range(0, e_rows)]
+for i in tqdm(range(0, e_rows), desc="Calculating", ascii=False, ncols=76):
+    for j in range(0, e_cols):
+        for k in range(0, t_rows):
+            for l in range(0, t_cols):
+                euclidean_data[i][j] += abs(data_full[i+k][j+l] - data_crop[k][l])
+print('Complete.')
 
-main()
+full_img = plt.imshow(euclidean_data)
+plt.show()
